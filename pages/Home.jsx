cd076aed6be34616006fe390b20c08a35eaf71a9
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-22 20:25:34 +0600

    d-1,setup,outlet and react router dom using,navigate path

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,4 +1,4 @@
-import { AppstoreOutlined} from '@ant-design/icons';
+import { UsergroupAddOutlined,ProductOutlined,MenuUnfoldOutlined,PicCenterOutlined} from '@ant-design/icons';
 import { Menu, Row, Col,   } from 'antd';
 import { Outlet, useNavigate } from 'react-router-dom';
 function getItem(label, key, icon, children, type) {
@@ -11,28 +11,28 @@ function getItem(label, key, icon, children, type) {
   };
 }
 const items = [
-  getItem('Users', 'sub1', <AppstoreOutlined />, [
+  getItem('Users', 'sub1', <UsergroupAddOutlined />, [
     getItem('Merchant', '/login'),
     getItem('Users', '2'),
      ]),
   {
     type: 'divider',
   },
-  getItem('Product', 'sub2', <AppstoreOutlined />, [
+  getItem('Product', 'sub2', <ProductOutlined />, [
     getItem('Add Product', '3'),
     getItem('All Product', '4'),
      ]),
   {
     type: 'divider',
   },
-    getItem('Category', 'sub3', <AppstoreOutlined />, [
+    getItem('Category', 'sub3', <MenuUnfoldOutlined />, [
     getItem('Add Category', '5'),
     getItem('All Category', '6'),
      ]),
   {
     type: 'divider',
   },
-    getItem('SubCategory', 'sub4', <AppstoreOutlined />, [
+    getItem('SubCategory', 'sub4', <PicCenterOutlined />, [
     getItem('Add SubCategory', '7'),
     getItem('All SubCategory', '8'),
      ]),
