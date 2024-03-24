bfd25d24753cc6f251c667240ed5617d680d2fcc
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-24 17:25:48 +0600

    Draft -js setup AddProduc

--- pages/AddProduct.jsx
+++ pages/AddProduct.jsx
@@ -1,13 +1,31 @@
+
 import Input from "antd/es/input/Input";
-
-
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState, convertToRaw } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
+import { useState } from "react";
 export default function AddProduct() {
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+      const onEditorStateChange = (editorState) => {
+        setEditorState(editorState);
+      };
+      
   return (
+    
     <div>
       <h2>Product Name :</h2>
       <Input placeholder="Product Name"/>
+
+      {/* Product Description */}
       <h2>Product  Description  :</h2>
-      
+      <Editor
+        editorState={editorState}
+        wrapperClassName="demo-wrapper"
+        editorClassName="demo-editor"
+        onEditorStateChange={onEditorStateChange}
+      />
+     
     </div>
   )
 }
