ef69d6b7b78e1d2e5ef7b53cc5e76a99abf1a580
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-05-19 21:19:17 +0600

    AddVairant Page- solved img url path, solved post img database

--- pages/AddVariant.jsx
+++ pages/AddVariant.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, Select } from 'antd'
+import { Alert, Button, Input, Select } from 'antd'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
@@ -24,88 +24,121 @@ export default function AddVariant() {
         );
     };
 
-    const [img, setImg] = useState("");
+    const [imgs, setImg] = useState("");
     const [productData, setProductData] = useState([]);
     const [productName, setProductName] = useState("");
-    const [addProduct , setaddProduct] = useState([]);
+    const [addProduct, setaddProduct] = useState([]);
+    const [alertSuccess,setAlertSuccess] = useState(false)
     useEffect(() => {
-      const fetchProduct = async () => {
-        try {
-            const arr = []
-          const response = await axios.get("http://localhost:7000/api/v1/allget/getproduct")
-        response.data.map((item)=>{           
-            arr.push({
-                value:item._id,
-                label: item.name
-            })
-        })
-         setaddProduct(arr)
-        } catch (error) {
-          console.log("Error Fethcing Product Data", error);
+        const fetchProduct = async () => {
+            try {
+                const arr = []
+                const response = await axios.get("http://localhost:7000/api/v1/allget/getproduct")
+                response.data.map((item) => {
+                    arr.push({
+                        value: item._id,
+                        label: item.name
+                    })
+                })
+                setaddProduct(arr)
+            } catch (error) {
+                console.log("Error Fethcing Product Data", error);
+            }
         }
-      }
-      fetchProduct()
+        fetchProduct()
     }, [productData])
 
-    const [variantData,setVariantData] = useState({
-        color : '',
-        ram : "" ,
-        storage : "",
-        size : "",
+    const [variantData, setVariantData] = useState({
+        color: '',
+        ram: "",
+        storage: "",
+        size: "",
         price: "",
-        quantity : ""
+        quantity: ""
     });
-  
-   const handleInputChange = (e)=>{
-    setVariantData({...variantData,[e.target.name]:e.target.value})
-   }
-   const handleImg =(e)=>{
-    setImg(e.target.files[0]);
-   }
-   console.log(variantData);
-  const handleCreateVariant = async ()=>{
-   await axios.post("http://localhost:7000/api/v1/become/createVariant",{
-   ram: variantData.ram,
-    storage : variantData.storage,
-     color : variantData.color ,
-      price : variantData.price, 
-     size : variantData.size, 
-     quantity : variantData.quantity,
-     img: img,
-      productId : productName
-   },
-   {
-    headers: {'Content-Type': 'multipart/form-data'}        
-   })
-  }
+
+    const handleInputChange = (e) => {
+        setVariantData({ ...variantData, [e.target.name]: e.target.value })
+    }
+    const handleImg = (e) => {
+        setImg(e.target.files[0]);
+    }
+
+    const handleCreateVariant = async () => {
+        try {
+            await axios.post("http://localhost:7000/api/v1/become/createVariant", {
+                ram: variantData.ram,
+                storage: variantData.storage,
+                color: variantData.color,
+                price: variantData.price,
+                size: variantData.size,
+                quantity: variantData.quantity,
+                img: imgs,
+                productId: productName
+            },
+                {
+                    headers: { 'Content-Type': 'multipart/form-data' }
+                })
+                setVariantData({
+                    color: '',
+                    ram: "",
+                    storage: "",
+                    size: "",
+                    price: "",
+                    quantity: ""
+                });
+                setProductName("");
+                setAlertSuccess(true)
+             
+        } catch (error) {
+       console.log(error.response.data);
+        }
+     // Hide the alert after 1 second
+     setTimeout(() => {
+        setAlertSuccess(false);
+    }, 1500);
+    }
+
     return (
-        <div style={{width:800}}>
+      <>
+       {
+                  alertSuccess &&
+                  <Alert
+                 style={{marginTop: 20}}
+                  message="Upload Successfuly"
+                  type="success"
+                  showIcon
+              />
+             }
+        <div style={{ width: 800 }}>                      
             <h3>Product Name : </h3>
             <Select
-            placeholder="select product"
-                 onChange={(e)=>setProductName(e)}
+                placeholder="select product"
+                onChange={(e) => setProductName(e)}
                 mode="single"
+                value={productName}
                 tagRender={tagRender}
                 style={{
                     width: '100%',
                 }}
-            options={addProduct}
+                options={addProduct}
             />
             <h3>Variant Color :</h3>
-            <Input onChange={handleInputChange} name='color' placeholder='color' />
+            <Input onChange={handleInputChange} value={variantData.color} name='color' placeholder='color' />
             <h3>Variant Ram :</h3>
-            <Input onChange={handleInputChange} name='ram' placeholder='ram' />
-              <h3>Variant Storage/Rom :</h3>
-            <Input onChange={handleInputChange} name='storage' placeholder='rom/storage' /> 
-             <h3>Variant Size :</h3>
-            <Input onChange={handleInputChange} name='size' placeholder='size' />
+            <Input onChange={handleInputChange} value={variantData.ram} name='ram' placeholder='ram' />
+            <h3>Variant Storage/Rom :</h3>
+            <Input onChange={handleInputChange} value={variantData.storage} name='storage' placeholder='rom/storage' />
+            <h3>Variant Size :</h3>
+            <Input onChange={handleInputChange} value={variantData.size} name='size' placeholder='size' />
             <h3>Variant Price :</h3>
-            <Input onChange={handleInputChange} name='price' placeholder='price' />
+            <Input onChange={handleInputChange} value={variantData.price} name='price' placeholder='price' />
             <h3>Variant Quantity :</h3>
-            <Input onChange={handleInputChange} name='quantity' placeholder='quantity' />
+            <Input onChange={handleInputChange} value={variantData.quantity} name='quantity' placeholder='quantity' />
             <h3>Variant Image :</h3>
             <Input onChange={handleImg}  type='file' placeholder='give me img ' />
-            <Button onClick={handleCreateVariant} type='primary' style={{marginTop: 20}}>Create Variant</Button>
+            <Button onClick={handleCreateVariant} type='primary' style={{ marginTop: 20 }}>Create Variant</Button>
         </div>
+      </>
     )
 }
